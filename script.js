a375250a6f5c753da76150287ca30c3ec70a89ae
Avery Hopkins <student27.gs4t@gmail.com>
2025-05-31 14:49:15 -0700

    almost added functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+let myname="Avery";
+let myage=13; 
+let likesushi=true; 
+
+function greeter(myname) {
+    console.log("Hello, my name is",myname)
+}
+
+greeter("Avery")
+
+let newname = document.getElementById("greeter").value;
